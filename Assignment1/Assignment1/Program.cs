901a7839cc27ba34d12bfe298b6f6c3362bc1164
jokel1973 <jordan.kelley@gmail.com>
2024-02-07 18:31:34 -0800

    First Assignment

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Displaying the welcome message
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+
+        // Displaying the additional message
+        Console.WriteLine("Remember, we’re \"accounting\" on you!");
+
+        // Keeps console open to be read
+        Console.ReadLine();
+    }
+}
+
