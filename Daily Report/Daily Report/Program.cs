abb0ec048877f65058152950b6f95a90d511a5fc
jokel1973 <jordan.kelley@gmail.com>
2024-02-08 17:40:15 -0800

    Daily Report assignment

---
+++ Daily Report/Daily Report/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+class DailyReport
+{
+    static void Main()
+    {
+        // Print the starting message
+        Console.WriteLine("Code Academy");
+        Console.WriteLine("Student Daily Report");
+
+        // Declare variables to store user input
+        string name, course, positiveExperiences, additionalFeedback, needHelp;
+        int pageNumber, studyHours;
+        
+
+        // Ask and save user data
+        Console.Write("What is your name? ");
+        name = Console.ReadLine();
+
+        Console.Write("What course are you on? ");
+        course = Console.ReadLine();
+
+        Console.Write("What page number? ");
+        // Use direct casting to int
+        pageNumber = (int)Convert.ChangeType(Console.ReadLine(), typeof(int));
+
+        Console.Write("Do you need help with anything? (true/false) ");
+        needHelp = Console.ReadLine();
+
+        Console.Write("Were there any positive experiences you’d like to share? Please give specifics. ");
+        positiveExperiences = Console.ReadLine();
+
+        Console.Write("Is there any other feedback you’d like to provide? Please be specific. ");
+        additionalFeedback = Console.ReadLine();
+
+        Console.Write("How many hours did you study today? ");
+        // Use direct casting to int
+        studyHours = (int)Convert.ChangeType(Console.ReadLine(), typeof(int));
+
+        // Print the closing message
+        Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.ReadLine();
+    }
+}
