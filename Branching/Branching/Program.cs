ca4cf0b893e0258dc2e706bcf2cd41647f6f88af
jokel1973 <jordan.kelley@gmail.com>
2024-02-12 14:30:04 -0800

    Branching

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        // Prompt user for package weight
+        Console.Write("Please enter the package weight: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        // Check if weight is greater than 50
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            Console.ReadLine();
+            return;// End the program
+            
+        }
+
+        // Prompt user for package dimensions
+        Console.Write("Please enter the package width: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please enter the package height: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please enter the package length: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        // Check if dimensions total greater than 50
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return; // End the program
+        }
+
+        // Calculate quote
+        double quote = (width * height * length * weight) / 100;
+
+        // Display the quote to the user
+        Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+        Console.WriteLine("Thank you!");
+        Console.ReadLine();
+    }
+}
