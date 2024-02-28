7850b2e12e659483e38829aae602cd9774d3b114
jokel1973 <jordan.kelley@gmail.com>
2024-02-28 11:53:50 -0800

    Void Method

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Void class
+            Void voidInstance = new Void();
+
+            // Call the MathOps method with two integers
+            voidInstance.MathOps(3, 7);
+
+            // Call the MathOps method with parameters specified by name
+            voidInstance.MathOps(firstNumber: 5, secondNumber: 10);
+
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Void.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Void
+    {
+        public void MathOps(int firstNumber, int secondNumber)
+        {
+            int squareResult = firstNumber * firstNumber;
+
+            Console.WriteLine($"Square of (firstNumber): {squareResult}, Second Number: {secondNumber}");
+        }
+    }
+}
