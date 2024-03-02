3e999bfa56b3f893d0934e9cd433d816f5f1363f
jokel1973 <jordan.kelley@gmail.com>
2024-03-01 17:04:52 -0800

    Polymorphism with Interface

---
+++ PolymorphismInterface/PolymorphismInterface/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person, IQuittable
+    {
+        // Implementation of the SayName method for Employee
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee Name: {FirstName} {LastName}");
+        }
+        // Implementation of the Quit method from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine($"Employee {FirstName} {LastName} has quit.");
+        }
+    }
+}
---
+++ PolymorphismInterface/PolymorphismInterface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Main program class
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate an Employee object with firstName "Sample" and lastName "Student"
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call the SayName() method on the Employee object
+            employee.SayName();
+
+            // Use polymorphism to create an object of type IQuittable
+            IQuittable quittableEmployee = employee;
+
+            // Call the Quit() method on the IQuittable object (polymorphism)
+            quittableEmployee.Quit();
+
+            // Keep the console window open until a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
