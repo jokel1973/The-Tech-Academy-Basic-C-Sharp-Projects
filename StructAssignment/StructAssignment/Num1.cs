590a044edad95dfc71317359fa51288b52e80940
jokel1973 <jordan.kelley@gmail.com>
2024-03-03 15:59:50 -0800

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Num1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Define a struct called Number
+    public struct Number
+    {
+        // Property of the struct, representing an amount of type decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create and initialize an object of data type Number
+            Number myNumber = new Number();
+
+            // Assign an amount (decimal value) to the Amount property of the struct
+            myNumber.Amount = 123.45M;
+
+            // Print the amount to the console
+            Console.WriteLine($"Amount: {myNumber.Amount}");
+
+            // Keep the console window open until a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
