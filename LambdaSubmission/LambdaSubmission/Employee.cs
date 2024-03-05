963baec3245a5df6d273960b6e68f7d5d25056fa
jokel1973 <jordan.kelley@gmail.com>
2024-03-04 17:12:16 -0800

    Lambda update

--- LambdaSubmission/LambdaSubmission/Employee.cs
+++ LambdaSubmission/LambdaSubmission/Employee.cs
@@ -12,5 +12,17 @@ namespace LambdaSubmission
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        // Overload the == operator to compare Employee IDs
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1?.Id == employee2?.Id;
+        }
+
+        // Overload the != operator to compare Employee IDs
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
     }
 }
