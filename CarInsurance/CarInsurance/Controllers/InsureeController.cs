38143ab8b8123234354f6c6b19bf2ef2e94d27c9
jokel1973 <jordan.kelley@gmail.com>
2024-03-25 17:17:30 -0700

    Car Insurance

---
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private readonly InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+
+        // GET: Insuree
+        public ActionResult Admin()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                // Start with a base of $50 / month
+                decimal monthlyTotal = 50;
+
+                // If the user is 18 or under, add $100 to the monthly total
+                if (DateTime.Now.Year - insuree.DateOfBirth.Year <= 18)
+                    monthlyTotal += 100;
+
+                // If the user is from 19 to 25, add $50 to the monthly total
+                else if (DateTime.Now.Year - insuree.DateOfBirth.Year >= 19 && DateTime.Now.Year - insuree.DateOfBirth.Year <= 25)
+                    monthlyTotal += 50;
+
+                // If the user is 26 or older, add $25 to the monthly total
+                else
+                    monthlyTotal += 25;
+
+                // If the car's year is before 2000, or after 2015 add $25 to the monthly total
+                if (int.TryParse(insuree.CarYear, out int carYear) && (carYear < 2000 || carYear > 2015))
+                {
+                    monthlyTotal += 25;
+                }
+
+               
+                // If the car's Make is a Porsche, add $25 to the price
+                if (insuree.CarMake.ToLower() == "porsche")
+                    monthlyTotal += 25;
+
+                // If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price
+                if (insuree.CarMake.ToLower() == "porsche" && insuree.CarModel.ToLower() == "911 carrera")
+                    monthlyTotal += 25;
+
+                // Add $10 to the monthly total for every speeding ticket the user has
+                monthlyTotal += insuree.SpeedingTickets * 10;
+
+                // If the user has ever had a DUI, add 25% to the total
+                if (insuree.DUI)
+                    monthlyTotal *= 1.25m;
+
+                // If it's full coverage, add 50% to the total
+                if (insuree.CoverageType)
+                    monthlyTotal *= 1.5m;
+
+                insuree.Quote = monthlyTotal;
+
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
