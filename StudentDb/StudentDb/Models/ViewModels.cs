065337fafeffa9c849b4aaf838448e3f195d1cb1
jokel1973 <jordan.kelley@gmail.com>
2024-03-27 18:01:36 -0700

    multiple app upload

---
+++ StudentDb/StudentDb/Models/ViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDb.Views
+{
+    public class ViewModels
+    {
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public byte[] Photo { get; set; }
+            public decimal Height { get; set; }
+            public float Weight { get; set; }
+
+            public Grade Grade { get; set; }
+        }
+
+        public class Grade
+        {
+            public int GradeId { get; set; }
+            public string GradeName { get; set; }
+            public string Section { get; set; }
+
+            public ICollection<Student> Students { get; set; }
+        }
+    }
+}
