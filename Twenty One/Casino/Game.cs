eea0a5ea2a12f1b097559f88cc00342ef334a4f2
jokel1973 <jordan.kelley@gmail.com>
2024-03-29 14:25:31 -0700

    Twenty One

---
+++ Twenty One/Casino/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public abstract class Game
+    {
+        // next two lines instantiate the player list 
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
+        public List<Player> Players { get { return _players; } set { _players = value; } }
+        public string Name { get; set; }
+       public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } }
+
+        public abstract void Play();// States that any class inheriting this class must implement this method
+
+        public virtual void ListPlayers() //adding keyword virtual makes it a virtual method, gets inherited, but can be overridden
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
